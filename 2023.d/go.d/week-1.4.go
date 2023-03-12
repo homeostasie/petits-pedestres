24d6c041de8f05cd1cf92909884832ba760729b8
homeostasie <986.thom@gmail.com>
2023-03-12 18:04:25 +0100

    week 4

---
+++ 2023.d/go.d/week-1.4.go
@@ -0,0 +1,31 @@
+package main
+import (
+    "fmt"
+    "encoding/json"    
+)
+
+func main() {
+	var Personne = make(map[string]string)
+
+	var monNom string
+	var monAdresse string
+
+	fmt.Println("Votre nom ?")
+	fmt.Scanln(&monNom)
+
+	fmt.Println("Votre addresse ?")
+	fmt.Scanln(&monAdresse)
+
+
+	Personne["Nom"] = monNom
+	Personne["Addresse"] = monAdresse
+
+
+	json, err := json.MarshalIndent(Personne, "", "\t")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(json))
+}
